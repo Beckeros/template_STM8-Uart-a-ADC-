7874e94a876c832f7dc49ada3e23333e228ff857
Albert Vychodil <93586840+Beckeros@users.noreply.github.com>
2022-01-04 12:10:36 +0100

    Initialize sdccrm

---
+++ src/main.c
@@ -0,0 +1,64 @@
+#include "stm8s.h"
+#include "milis.h"
+
+/*#include "delay.h"*/
+/*#include <stdio.h>*/
+/*#include "../lib/uart.c"*/
+
+#define _ISOC99_SOURCE
+#define _GNU_SOURCE
+
+#define LED_PORT GPIOC
+#define LED_PIN  GPIO_PIN_5
+#define LED_ON   GPIO_WriteHigh(LED_PORT, LED_PIN);
+#define LED_OFF  GPIO_WriteLow(LED_PORT, LED_PIN);
+#define LED_FLIP GPIO_WriteReverse(LED_PORT, LED_PIN);
+
+#define BTN_PORT GPIOE
+#define BTN_PIN  GPIO_PIN_4
+#define BTN_PUSH (GPIO_ReadInputPin(BTN_PORT, BTN_PIN)==RESET)
+
+
+void init(void)
+{
+    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
+    GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+    init_milis();
+
+}
+
+/*int8_t abc(void) {*/
+/*    static int8_t x = 55;*/
+/*    return x+154;*/
+/*}*/
+
+int main(void)
+{
+    uint32_t time = 0;
+    init();
+    /*init_uart();*/
+
+    while (1) {
+
+            GPIO_WriteReverse(GPIOC, LED_PIN);
+            GPIO_WriteReverse(GPIOC, LED_PIN);
+            GPIO_WriteReverse(GPIOC, LED_PIN);
+            GPIO_WriteReverse(GPIOC, LED_PIN);
+            GPIO_WriteReverse(GPIOC, LED_PIN);
+            GPIO_WriteReverse(GPIOC, LED_PIN);
+            GPIO_WriteReverse(GPIOC, LED_PIN);
+            GPIO_WriteReverse(GPIOC, LED_PIN);
+            GPIO_WriteReverse(GPIOC, LED_PIN);
+            GPIO_WriteReverse(GPIOC, LED_PIN);
+            GPIO_WriteReverse(GPIOC, LED_PIN);
+            GPIO_WriteReverse(GPIOC, LED_PIN);
+        
+
+        /*LED_FLIP; */
+        /*_delay_ms(333);*/
+        /*printf("Funguje to!!!\n");*/
+    }
+}
+
+/*-------------------------------  Assert -----------------------------------*/
+#include "__assert__.h"
